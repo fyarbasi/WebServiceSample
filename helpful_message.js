eb2d33a52c2dc16b485c1a6220e82e244a794514
fyarbasi <fyarbasi@yahoo.com>
2013-02-20 15:29:59 +0200

    added new js function

---
+++ helpful_message.js
@@ -0,0 +1,10 @@
+﻿function every()
+{
+    var appId = 0;
+    var b = appId;
+    for (var a = 0; a < 10 ; a++)
+    {
+        b = b + a;
+    }
+    document.getElementById('txtFyarbasi').innerText = b.toString();
+}
